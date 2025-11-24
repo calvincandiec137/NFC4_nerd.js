b381480b1d66a3d4dbbcf30cddb9b157a167ccf7
Faiz <khanfaizmominshama@gmail.com>
2025-11-24 22:09:51 +0530

    added cors

--- modules/app.py
+++ modules/app.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import base64
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (or specify your frontend URL)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
